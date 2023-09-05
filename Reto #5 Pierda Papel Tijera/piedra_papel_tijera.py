a805cfcd30310aa8be4a93e764cec6017ce777aa
Eriosoul <tboss1780@gmail.com>
2023-09-05 14:46:13 +0200

    piedra papel tijera

--- Reto #5 Pierda Papel Tijera/piedra_papel_tijera.py
+++ Reto #5 Pierda Papel Tijera/piedra_papel_tijera.py
@@ -8,3 +8,34 @@
  * - Ejemplo. Entrada: [("🗿","✂️"), ("✂️","🗿"), ("📄","✂️")]. Resultado: "Player 2".
  * - Debes buscar información sobre cómo se juega con estas 5 posibilidades.
 """
+
+
+def rock_paper_scissor(games):
+    # Comprobacion de quien gana, uso de matriz
+    rules: dict = {"🗿": ["✂", "🦎"],
+                   "🧻": ["🗿", "🖖"],
+                   "✂": ["🖖", "🦎"],
+                   "🦎": ["🖖", "🧻"],
+                   "🖖": ["🗿", "✂"]
+                   }
+
+    player_one = 0
+
+    player_tow = 0
+
+    for game in games:
+        player_one_game = game[0]
+        player_tow_game = game[1]
+        if player_one_game != player_tow_game:
+            if player_tow_game in rules[player_one_game]:
+                player_one += 1
+            else:
+                player_tow += 1
+
+    return "Tie" if player_one == player_tow else "Player 1 won" if player_one > player_tow else "Player 2 won"
+
+
+print(rock_paper_scissor([("🗿", "🧻"),("🖖", "🦎"),("🦎", "🦎"),("🗿", "🖖"),("🗿", "✂"),("🦎", "🦎"),
+                          ("✂", "🧻"),("🗿", "🖖"),("🗿", "✂"),("🗿", "🦎"),("🗿", "✂"),("🗿", "🧻")]
+                         ))
+
